4b8b8267b1d30f0df94cb3ad42e0b84a18ba8c80
Josh Thompson <jt244345@falmouth.ac.uk>
2022-07-23 03:30:35 +0100

    Moved homebutton opacity to styles.css and changed bg colour

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menuLinks = document.querySelector('.navbar-menu')
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
